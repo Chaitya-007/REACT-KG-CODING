ff39683bc83ca02a34564c7b1cc720bf43f578cb
Chaitya <chaitanyaasole@gmail.com>
2024-08-18 16:56:12 +0530

    Added row compoent

--- Projects/1-todo-app-version-one/src/App.jsx
+++ Projects/1-todo-app-version-one/src/App.jsx
@@ -1,24 +1,12 @@
 import AppName from "./components/AppName";
+import AddTodo from "./components/AddTodo";
 
 function App() {
   return (
     <center class="todo-container">
       <AppName />
+      <AddTodo />
       <div class="container text-center">
-        <div class="row">
-          <div class="col-6">
-            <input type="text" placeholder="Enter Text here" />
-          </div>
-          <div class="col-4">
-            <input type="date" />
-          </div>
-          <div class="col-2">
-            <button type="button" class="btn btn-success">
-              Add
-            </button>
-          </div>
-        </div>
-
         <div class="row">
           <div class="col-6">Buy Milk</div>
           <div class="col-4">4/10/2023</div>
