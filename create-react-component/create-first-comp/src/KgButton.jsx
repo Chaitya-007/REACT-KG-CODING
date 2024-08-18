eca19ebadaf09674f33506a20b3429cae4bae16a
Chaitya <chaitanyaasole@gmail.com>
2024-08-18 10:46:05 +0530

    Ways to export functions

--- create-react-component/create-first-comp/src/KgButton.jsx
+++ create-react-component/create-first-comp/src/KgButton.jsx
@@ -1,3 +1,18 @@
+// Ways to export function
+// export default function MyButton() {
+//   return;
+// }
+
+// way 2
+// Mulitple named exports
+// export function MyButton() {
+//   return;
+// }
+// While importing
+// import { MyButton2, MyButton3} from ...
+
+// way 3
+
 function KgButton() {
   return <button>Click me</button>;
 }
