c977f184ead78ecc2a508e7819d4cb32460e0708
Chaitya <chaitanyaasole@gmail.com>
2024-08-18 15:45:25 +0530

    Imported card

--- 017_BootStrap/learning-bootstrap/src/App.jsx
+++ 017_BootStrap/learning-bootstrap/src/App.jsx
@@ -1,27 +1,19 @@
 import "./App.css";
-import pandaImage from "./assets/panda.png";
+import pandaImage from "./images/panda.png";
 
 function App() {
   return (
-    // {can you please resolve the image error}
-    // {Sure, I can help you with that.}
-    // {I have imported the image in the App component.}
-    // {I have also added the image in the card.}
-    // {Now, you can see the image in the card.}
-
-    <div>
-      <div class="card" style="width: 18rem;">
-        <img src={pandaImage} class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" class="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
+    <div className="card" style={{ width: "18rem" }}>
+      <img src={pandaImage} className="card-img-top" alt="this is image" />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a href="#" className="btn btn-primary">
+          Go somewhere
+        </a>
       </div>
     </div>
   );
