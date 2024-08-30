e6ec4ffc2c13fdcfb3830000234b2d37def62544
Chaitya <chaitanyaasole@gmail.com>
2024-08-30 17:07:48 +0530

    Conditional rendering

--- 018_Fragments/learning-fragments/src/App.jsx
+++ 018_Fragments/learning-fragments/src/App.jsx
@@ -3,10 +3,20 @@ import "./App.css";
 
 function App() {
   let foodItems = ["Apple", "Banana", "Orange", "Grapes", "Mango", "Green"];
+  // let foodItems = [];
+
+  // if (foodItems.length === 0) {
+  //   return <p>No items found</p>;
+  // }
+
+  let value = foodItems.length === 0 ? <p>No items found</p> : null;
 
   return (
     <>
       <h1>Headlthy Food</h1>
+      {/* {foodItems.length === 0 ? <p>No items found</p> : null} */}
+      {/* {value} */}
+      {foodItems.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {foodItems.map((item) => (
           <li key={item} className="list-group-item">
