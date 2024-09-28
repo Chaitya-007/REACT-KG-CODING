11e11c316ced642400c4ae91d6b88b71da2f7c21
Chaitanya <chaitanyaasole@gmail.com>
2024-09-28 14:46:22 +0530

    implemented useReducer

--- Projects/6-todo-app-version-3/src/App.jsx
+++ Projects/6-todo-app-version-3/src/App.jsx
@@ -2,31 +2,60 @@ import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
 import TodoItems from "./components/TodoItems";
 import "./App.css";
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import WelcomeMessage from "./components/WelcomeMessage";
 import { TodoItemsContext } from "./store/todo-items-store";
 
+const todoItemsReducer = (currTodoItems, action) => {
+  let newTodoItems = currTodoItems;
+
+  if (action.type === "NEW_ITEM") {
+    console.log(
+      `New Item Added : ${action.payload.itemName} with due date : ${action.payload.itemDueDate}`
+    );
+
+    newTodoItems = [
+      ...currTodoItems,
+      { name: action.payload.itemName, duedate: action.payload.itemDueDate },
+    ];
+  } else action.type === "DELETE_TYPE";
+  {
+    newTodoItems = currTodoItems.filter(
+      (item) => item.name !== action.payload.itemName
+    );
+    console.log(`Deleted ${action.payload.itemName}`);
+  }
+
+  return newTodoItems;
+};
+
 function App() {
-  let [todoItems, setToDoItems] = useState([]);
+  // let [todoItems, setToDoItems] = useState([]);
   // let [todoDate, setTodoDate] = useState("");
 
+  const [todoItems, dispatchTodoItems] = useReducer(todoItemsReducer, []);
+
   const handleNewItem = (itemName, itemDueDate) => {
-    console.log(`New Item Added : ${itemName} with due date : ${itemDueDate}`);
-    // const newTodoitems = [
-    //   ...todoItems,
-    //   { name: itemName, duedate: itemDueDate },
-    // ];
-    // setToDoItems(newTodoitems);
-    setToDoItems((currValue) => [
-      ...currValue,
-      { name: itemName, duedate: itemDueDate },
-    ]);
+    const newItemAction = {
+      type: "NEW_ITEM",
+      payload: {
+        itemName,
+        itemDueDate,
+      },
+    };
+
+    dispatchTodoItems(newItemAction);
   };
 
   const delmsg = (itemName) => {
-    const newTodoItems = todoItems.filter((item) => item.name !== itemName);
-    setToDoItems(newTodoItems);
-    console.log(`Deleted ${itemName}`);
+    const deleteItemAction = {
+      type: "DELETE_TYPE",
+      payload: {
+        itemName: itemName,
+      },
+    };
+
+    dispatchTodoItems(deleteItemAction);
   };
 
   // const todoItems = [
