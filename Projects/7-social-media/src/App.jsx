760605d86779a0e6e4fea5d157ba7bb503240fbf
Chaitanya <chaitanyaasole@gmail.com>
2024-09-29 21:27:48 +0530

    Made Sidebar component

--- Projects/7-social-media/src/App.jsx
+++ Projects/7-social-media/src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <>
+      <Sidebar></Sidebar>
       <Header></Header>
       <Footer></Footer>
     </>
